d9ec3fec1f73d9f95c461c859286ffe8dbc4890e
jjhurtadoa <51895444+jjhurtadoa@users.noreply.github.com>
2022-05-02 12:44:39 -0500

    fixed

--- preguntas.py
+++ preguntas.py
@@ -115,7 +115,7 @@ def pregunta_04():
     from sklearn.metrics import mean_squared_error
 
     # Lea el archivo `gm_2008_region.csv` y asignelo al DataFrame `df`
-    df = df = pd.read_csv('gm_2008_region.csv')
+    df = pd.read_csv('gm_2008_region.csv')
 
     # Asigne a la variable los valores de la columna `fertility`
     X_fertility = df['fertility'].values.reshape(-1, 1)
